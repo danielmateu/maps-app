70f24e4526685cd550a1f7f5e1fd23c9c31ecf68
Daniel Mateu <danielmateu86@gmail.com>
2023-04-20 14:52:17 +0200

    Estableciendo estado de nuestro contexto

---
+++ src/MapsApp.tsx
@@ -0,0 +1,10 @@
+
+import { PlacesProvider } from './context/places/PlacesProvider'
+
+export const MapsApp = () => {
+    return (
+        <PlacesProvider>
+            <h1>Hola mundo de nuevo</h1>
+        </PlacesProvider>
+    )
+}
