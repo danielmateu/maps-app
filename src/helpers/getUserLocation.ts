89aed21474a23621d26b8fc536c19061923d31be
Daniel Mateu <danielmateu86@gmail.com>
2023-04-24 11:35:01 +0200

    Fin proyecto!

--- src/helpers/getUserLocation.ts
+++ src/helpers/getUserLocation.ts
@@ -1,5 +1,5 @@
 
-export const getUserLocation = async (): Promise<number[]> => {
+export const getUserLocation = async (): Promise<[number, number]> => {
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(
             ({coords}) => {
